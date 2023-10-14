0b3e1936c2d265c5ab754ed393e0599e2c300bf3
zardari5647 <B00150460@mytudublin.ie>
2023-10-14 17:37:22 +0100

    game
    fetch game

---
+++ Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+
+    private float fireRate = 1; // time the player has to wait to fire again
+private float nextFire = 0; // time since start after which player can fire again
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+       {
+ nextFire = Time.time + fireRate; // reset nextFire to current time + fireRate
+ Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+       }
+
+    }
+}
